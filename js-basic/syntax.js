3b873abea956bcef58a91167c6c3f3d4a1899e60
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-02 10:35:58 +0700

    js-tutorial changed to js-basic

---
+++ js-basic/syntax.js
@@ -0,0 +1,33 @@
+// cara membuat variable
+let x;
+let y;
+
+// cara menggunakan variable
+x = 5;
+y = 5;
+let z = x * y;
+
+// JavaScript Values
+/**
+ * JavaScript memiliki 2 jenis nilai (values):
+ * 1. Fixed Values -> disebut Literal Values
+ * 2. Variable Values -> disebut Variables
+ */
+
+// Literal Values
+const number = 42;      // 42 adalah literal value 
+const text = "Hello";   // "Hello" adalah literal value
+// singkat nya adalah literal value itu gk bisa diubah sesuai namanya itu fixed value
+
+// Variables
+let age = 25;           // age adalah variable, 25 adalah literal
+age = 30;               // nilai age berubah jadi 30
+
+//Javascript operators
+//JavaScript menggunakan operator aritmatika ( + - * /) untuk menghitung nilai:
+(2 + 8) * 20
+
+//JavaScript menggunakan assignment operator (=) untuk menetapkan nilai ke variabel:
+let a, b;
+a = 50;
+b= 20;
