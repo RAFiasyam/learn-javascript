8f002979ada1366c6f0f1ec927049b9b03443e28
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-02 09:50:59 +0700

    statements

---
+++ js-tutorial/statements.js
@@ -0,0 +1,11 @@
+let x, y, z;
+x = 5;
+y = 32;
+z = x + y;
+
+document.getElementById("result1").innerHTML = "Jadi nilai dari z adalah " + z + ".";
+
+// jika ini ingin menggunakan button "see the answer"
+// function jawaban() {
+//     document.getElementById("result1").innerHTML = "Jadi nilai dari z adalah " + z + ".";
+// }
