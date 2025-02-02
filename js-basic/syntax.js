d41af9fbb8e379feb4398d8abc882b09605c9848
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-02 11:43:19 +0700

    syntax.js added before and updated

--- js-basic/syntax.js
+++ js-basic/syntax.js
@@ -30,4 +30,33 @@ age = 30;               // nilai age berubah jadi 30
 //JavaScript menggunakan assignment operator (=) untuk menetapkan nilai ke variabel:
 let a, b;
 a = 50;
-b= 20;
+b = 20;
+
+// Javascript Indentifies/ Names
+/**
+ *
+ * Dentifiers adalah nama javascript.
+ * Identifier digunakan untuk menyebutkan variabel dan kata kunci, dan fungsi.
+ * Aturan untuk nama hukum sama di sebagian besar bahasa pemrograman.
+ *
+ * Nama JavaScript harus dimulai dengan:
+ *    1. Letter (A-Z atau A-Z)
+ *    2. Dollar sign ($)
+ *    3. Atau underscore (_)
+ *
+ * Karakter selanjutnya dapat berupa letters, digits, underscores, atau dolar signs.
+ */
+
+
+// JavaScript and Camel Case
+/** 
+ * Secara historis, pemrogram telah menggunakan berbagai cara untuk bergabung dengan banyak kata menjadi satu nama variabel:
+ *  `Underscore:`
+ *  first_name, last_name, master_card, inter_city.
+ * 
+ *  `Upper Camel Case (pascal case):`
+ *  FirstName, LastName, MasterCard, InterCity.
+ * 
+ *  `Lower Camel Case:`
+ *  firstName, lastName, masterCard, interCity. //JavaScript programmers tend to use camel case that starts with a lowercase letter
+ */
