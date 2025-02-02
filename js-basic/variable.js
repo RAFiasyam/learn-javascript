05e2ace87a33c39e26f69420e52dc6f52ffb75c3
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-02 17:15:23 +0700

    variable.js added

---
+++ js-basic/variable.js
@@ -0,0 +1,41 @@
+/*
+    Variabel adalah container untuk menyimpan data
+    Variabel JavaScript dapat di declared dalam 4 cara:
+
+    1.Automactily
+    2.var
+    3.let
+    4.const
+*/
+
+// contoh dari Automaticly
+x = 5;
+y = 6;
+z = x + y;
+
+// contoh dari var
+var x = 5;
+var y = 6;
+var z = x + y;
+
+// contoh dari let
+let x = 5;
+let y = 6;
+let z = x + y;
+
+// contoh dari const
+const x = 5;
+const y = 6;
+const z = x + y;
+
+//Upper Camel Case (pascal case)
+FirstName = "Rafi";
+var FirstName = "Rafi";
+let FirstName = "Rafi";
+const FirstName = "Rafi";
+
+// Lower Camel Case
+firstName = "Rafi";
+var firstName = "Rafi";
+let firstName = "Rafi";
+const firstName = "Rafi";
